4539ab0901bbfa4252e83c27a59ab3e0b69e7b22
maanav <shah.maanav.07@gmail.com>
2021-01-24 13:35:35 +0530

    Add frontend support for CRUD on Leads

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,14 +4,19 @@ import ReactDOM from 'react-dom'
 import Header from './layout/Header'
 import Dashboard from './leads/Dashboard'
 
+import { Provider } from 'react-redux'
+import store from '../store'
+
 class App extends Component {
     render() {
         return (
-            <Fragment>
-                <Header />
-                <div className="container"></div>
-                <Dashboard />
-            </Fragment>
+            <Provider store={store}>
+                <Fragment>
+                    <Header />
+                    <div className="container"></div>
+                    <Dashboard />
+                </Fragment>
+            </Provider>
         )
     }
 }
